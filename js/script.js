29ba75eaf911cc9145c6873ddd58c90a2b15c5f1
Guilherme <guilhermemoraes1826@gmail.com>
2022-09-18 12:27:30 -0300

    Updating

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 
-let inputText = document.getElementById("campo");
-let numberValue = inputText.value
-let paragraph = document.createElement("p");
-let text = document.createTextNode("The number converted to decimal is: ");
+let btnSubmit = document.querySelector("#btnSubmit");
 
-paragraph.appendChild(text);
-document.body.appendChild(paragraph);
+btnSubmit.addEventListener("click", function(event){
+    event.preventDefault();
+
+    let frmNumber = document.querySelector("#formNumber");
+    let binaryNumber = frmNumber.name.value;
+    binaryNumber = parseInt(binaryNumber);
+    binaryNumber += 1
+    document.querySelector("#result").innerHTML += binaryNumber;
+
+})
