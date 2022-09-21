48dea162f2fffd1f91126cdff8ced35d11063567
Guilherme <guilhermemoraes1826@gmail.com>
2022-09-20 22:19:03 -0300

    updating files

--- js/script.js
+++ js/script.js
@@ -4,10 +4,15 @@ let btnSubmit = document.querySelector("#btnSubmit");
 btnSubmit.addEventListener("click", function(event){
     event.preventDefault();
 
-    let frmNumber = document.querySelector("#formNumber");
-    let binaryNumber = frmNumber.name.value;
-    binaryNumber = parseInt(binaryNumber);
-    binaryNumber += 1
+    const frmNumber = document.querySelector("#formNumber");
+    const userInput = frmNumber.name.value;
+    const binaryNumber = [userInput.length]
+    // let sizeBinaryNumber = binaryNumber.length;
+
+    for (let i = 0; i <= binaryNumber.length; i++){
+        console.log(i);
+    }
+    console.log(typeof(binaryNumber.length))
     document.querySelector("#result").innerHTML += binaryNumber;
 
 })
